d80d5da2ef236ce61cb63260f99e24f413c5da3f
Ravi Kant Prasad <63839002+ravi0818@users.noreply.github.com>
2020-12-03 00:50:04 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,109 @@
+//----------------class-----------------//
+
+$("#buttons>div").addClass("col-lg-2 col-md-4 col-sm-4");
+
+$("#obj>div").addClass("col-lg-4 col-md-6 col-sm-6");
+
+$("#obj>div>img").addClass("img-thumbnail image");
+
+
+
+//-----------------text--------------------//
+$("title").text("SuperDeals");
+
+
+
+
+//-----------------function-----------------//
+$(".image").click(function(){
+	window.open($(this).attr("src"), "_self");
+});
+
+//$(document).ready(function(){   
+//   $("#buttons").load("../snippet/buttons.html");
+//});
+
+
+/*$("button").click(function(){
+	localStorage.setItem("text",$(this).text());
+	let x=$(this).text()
+	y=x+".html"
+	window.open(y, "_self");
+});*/
+
+$( document ).ready(function(){
+	if($("h1").text()!=="Home"){
+		$("h1").text(localStorage.getItem("text"));
+	}
+});
+
+//----------------------load-------------------------
+$(function(){
+	$(".data1").load("amzBE.txt"); 
+});
+$(function(){
+	$(".data2").load("amzBCA.txt"); 
+});
+$(function(){
+	$(".data3").load("amzBH.txt"); 
+});
+$(function(){
+	$(".data4").load(); 
+});
+$(function(){
+	$(".data5").load(); 
+});
+$(function(){
+	$(".data6").load(); 
+});
+
+function currentTime() {
+  var date = new Date(); /* creating object of Date class */
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  $("#clock").text(hour + " : " + min + " : " + sec); /* adding time to the div */
+  var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
+}
+
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  }
+  else {
+    return k;
+  }
+}
+
+currentTime(); /* calling currentTime() function to initiate the process */
+// $(function(){
+// 	$(".data").load("data.txt"); 
+// });
+
+// $(function(){
+// 	$("#1").load("data1.txt"); 
+// });
+/*
+function arrayToTable(tableData) {
+        var table = $('<table></table>');
+        $(tableData).each(function (i, rowData) {
+            var row = $('<tr></tr>');
+            $(rowData).each(function (j, cellData) {
+                row.append($('<td>'+cellData+'</td>'));
+            });
+            table.append(row);
+        });
+        return table;
+    }
+
+    $.ajax({
+        type: "GET",
+        url: "C:\\Users\\rkpra\\Documents\\GitHub\\Web\\products.csv",
+        success: function (data) {
+            $('body').append(arrayToTable(Papa.parse(data).data));
+        }
+    });
+    */
