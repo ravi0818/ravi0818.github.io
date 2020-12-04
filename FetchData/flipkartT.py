5cd554cfb5b9185715ab222aaeb2507fca6ed6ce
ravi0818 <rkprasad0818@gmail.com>
2020-12-04 14:29:03 +0530

    update

--- FetchData/flipkartT.py
+++ FetchData/flipkartT.py
@@ -1,16 +1,65 @@
-from selenium import webdriver
 from bs4 import BeautifulSoup
+import selenium
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.common.exceptions import TimeoutException
 
-#--------------------------------------TopM.txt-------------------------------------------
-url="https://www.flipkart.com/offers-list/top-selling-mobiles?screen=dynamic&pk=themeViews%3DTopsellers-Mobiles-March18%3ADesk~widgetType%3DdealCard~contentType%3Dneo&wid=7.dealCard.OMU_5&otracker=clp_omu_Top%2BSelling%2BMobiles_offers-store_5&otracker1=clp_omu_PINNED_neo%2Fmerchandising_Top%2BSelling%2BMobiles_NA_wc_view-all_5"
-driver = webdriver.PhantomJS()
-driver.get(url)
-htmlcontent=driver.page_source
+#option.add_argument(" — incognito")
+browser = webdriver.Chrome(executable_path='C:\\WebDrivers\\chromedriver.exe')
+
+browser.get("https://ravi0818.github.io")
+# Wait 20 seconds for page to load
+timeout = 20
+try:
+	WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.TAG_NAME, "table")))
+	print(browser.find_element_by_tag_name('table'))
+except TimeoutException:
+	print("Timed out waiting for page to load")
+	browser.quit()
+#----------
+browser.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
+# driver.get('https://www.flipkart.com/offers-list/top-selling-mobiles?screen=dynamic&pk=themeViews%3DTopsellers-Mobiles-March18%3ADesk~widgetType%3DdealCard~contentType%3Dneo&wid=7.dealCard.OMU_5&otracker=clp_omu_Top%2BSelling%2BMobiles_offers-store_5&otracker1=clp_omu_PINNED_neo%2Fmerchandising_Top%2BSelling%2BMobiles_NA_wc_view-all_5');
+# driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+# print(driver.title)
+# s = driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div[2]')
+# print(s.text)
+# # search_box.send_keys('ChromeDriver')
+# # search_box.submit()
+# time.sleep(25) # Let the user actually see something!
+# driver.quit()
 
 # r=requests.get(url)
 # #writer=csv.writer(dataFile)
 # htmlcontent=r.content
-soup=BeautifulSoup(htmlcontent,'html.parser')
+# soup=BeautifulSoup(htmlcontent,'html.parser')
 # soup.prettify()
 # products=[] #List to store name of the product
 # prices=[] #List to store price of the product
@@ -19,7 +68,7 @@ soup=BeautifulSoup(htmlcontent,'html.parser')
 # images=[]
 # print(soup.find_all('a'))
 # with open("FktTopM.txt","w",encoding="utf-8",newline="") as dataFile:
-print(soup.find('a',class_='_6WQwDJ'))
+# print(soup.find('a',class_='_6WQwDJ'))
 # 	for a in soup.find_all('div',attrs={'class':'_1FNrEw'}):
 # 		print("test")
 # 		name=a.find('div', attrs={'class':'_3LU4EM'})
